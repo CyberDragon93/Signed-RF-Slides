c6299b3579a237fac361056110fa0f3bd2b3e2c4
Runlong Liao <102126402+CyberDragon93@users.noreply.github.com>
2026-08-05 00:31:58 -0700

    Narrow the FID chart further

--- components/RfImagenetFid.vue
+++ components/RfImagenetFid.vue
@@ -11,8 +11,8 @@ const props = defineProps({
 })
 
 const W = 900
-const PX0 = 150
-const PX1 = 660
+const PX0 = 200
+const PX1 = 630
 const PY0 = 42
 const PY1 = computed(() => props.height - 58)
 
